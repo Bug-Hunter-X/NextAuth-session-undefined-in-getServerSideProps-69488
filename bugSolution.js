608e689f84ae4eb84430b1dc575cf9833d543656
BugHunter-X <askzenai@gmail.com>
2025-01-17 23:00:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,28 @@
+```javascript
+import { unstable_getServerSession } from "next-auth/next";
+import { authOptions } from "pages/api/auth/[...nextauth]";
+
+export async function getServerSideProps(context) {
+  const session = await unstable_getServerSession(context, authOptions);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+}
+
+export default function Home({ session }) {
+  // ... rest of your component, use session object
+  console.log(session); 
+}
+```
